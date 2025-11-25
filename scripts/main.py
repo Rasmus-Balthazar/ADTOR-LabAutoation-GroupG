4db0ff019c847a5246f0a5c6aac83ba140cf9e95
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-11-25 16:09:22 +0100

    Feat : Color distance method

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,4 @@
+from color import Color
 from syringe import Syringe
 
 syringe = Syringe()
@@ -15,7 +16,11 @@ syringe.add_color(10)
 print(f"Syringe thinks it contains {syringe.mL}")
 print(f"Syringe says it readings are {syringe.get_rgb()}")
 
-print(syringe.get_rgb())
+rgb = syringe.get_rgb()
+actual_rgb = Color(rgb[0], rgb[1], rgb[2])
+goal_rgb = Color(0, 0, 0)
+print(rgb.distance(goal_rgb))
+
 
 print(f"Syringe thinks it contains {syringe.mL}")
 print(f"Syringe says it readings are {syringe.get_rgb()}")
