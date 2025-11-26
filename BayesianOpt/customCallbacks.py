0676cca3be55cc9acc605544a1ae7ca365d76c6c
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-11-26 10:21:28 +0100

    Format : Auto-formatting and removal of unused imports in BayesianOpt/

--- BayesianOpt/customCallbacks.py
+++ BayesianOpt/customCallbacks.py
@@ -1,12 +1,9 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from skopt import callbacks
 from time import time
 
 
-
 class PlotterCallback(callbacks.VerboseCallback):
-
     fx = None
     ax = None
 
@@ -21,30 +18,27 @@ class PlotterCallback(callbacks.VerboseCallback):
         self._print_info(start=True)
         self._plot_info(start=True)
 
-
     def _plot_info(self, start=True, y=0.0):
         iter_no = self.iter_no
         if start:
-            #Create graph
+            # Create graph
             self.f, self.ax = plt.subplots(figsize=(7, 7))
             plt.pause(0.5)
 
         if iter_no <= self.n_init:
-            self.ax.plot(iter_no,y,'go')
+            self.ax.plot(iter_no, y, "go")
             self.f.canvas.draw()
             plt.pause(0.5)
-        
 
         elif self.n_init < iter_no <= (self.n_random + self.n_init):
-            self.ax.plot(iter_no,y,'go')
+            self.ax.plot(iter_no, y, "go")
             self.f.canvas.draw()
             plt.pause(0.5)
 
         else:
-            self.ax.plot(iter_no,y,'go')
+            self.ax.plot(iter_no, y, "go")
             self.f.canvas.draw()
             plt.pause(0.5)
-            
 
     def __call__(self, res):
         """
@@ -53,15 +47,15 @@ class PlotterCallback(callbacks.VerboseCallback):
         res : `OptimizeResult`, scipy object
             The optimization as a OptimizeResult object.
         """
-        
+
         curr_y = res.func_vals[-1]
         curr_min = res.fun
 
         self.tracker.step = self.iter_no
 
         time_taken = time() - self._start_time
-        self._print_info(start = False)
-        self._plot_info(start = False,y=curr_y)
+        self._print_info(start=False)
+        self._plot_info(start=False, y=curr_y)
 
         print("Time taken: %0.4f" % time_taken)
         print("Function value obtained: %0.4f" % curr_y)
--- BayesianOpt/evaluateFitness.py
+++ BayesianOpt/evaluateFitness.py
@@ -1,26 +1,24 @@
-target = [0.5,0.5,0.5]
+target = [0.5, 0.5, 0.5]
 
 
 def mixAndMeasure(individual: tuple[int, int, int], generation: int, indvNumber):
     """Given an idea for a mix, run the pumps and return the resulting sensor reading"""
-    
+
     print(generation, indvNumber)
     grownIndividual = [i for i in individual]
 
     print("Evaluating:", grownIndividual)
-    
+
     return grownIndividual
 
+
 def evaluate(individual: tuple[int, int, int], generation: int, indvNumber) -> float:
-      
-    measure = mixAndMeasure(individual, generation, indvNumber) #Call a function to start the pumps and return a sensor value 
+    measure = mixAndMeasure(individual, generation, indvNumber)  # Call a function to start the pumps and return a sensor value
 
     print(f"{individual = }\n{measure = }\n{target = }")
     distances = [(t - m) ** 2 for (t, m) in zip(target, measure)]
 
     squaredError = sum(distances)
-    meanSquaredError = squaredError/(len(target))
+    meanSquaredError = squaredError / (len(target))
 
     return meanSquaredError
-
-
